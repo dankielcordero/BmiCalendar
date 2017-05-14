5fb49d44b8a989dc5948e191423a999389b134e2
Dankyel Cordero <dankielcordero@gmail.com>
2017-05-14 20:27:17 +0200

    commit2

--- app/src/main/java/cz/inovett/bmicalendar/WelcomeScreenActivity4.java
+++ app/src/main/java/cz/inovett/bmicalendar/WelcomeScreenActivity4.java
@@ -6,19 +6,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class WelcomeScreenActivity3 extends AppCompatActivity {
+public class WelcomeScreenActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome_screen3);
-
+        setContentView(R.layout.activity_welcome_screen4);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(WelcomeScreenActivity3.this, WelcomeScreenActivity4.class);
+                Intent i = new Intent(WelcomeScreenActivity4.this, WelcomeScreenActivity5.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/cz/inovett/bmicalendar/WelcomeScreenActivity5.java
+++ app/src/main/java/cz/inovett/bmicalendar/WelcomeScreenActivity5.java
@@ -6,19 +6,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class WelcomeScreenActivity3 extends AppCompatActivity {
+public class WelcomeScreenActivity5 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome_screen3);
-
+        setContentView(R.layout.activity_welcome_screen5);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(WelcomeScreenActivity3.this, WelcomeScreenActivity4.class);
+                Intent i = new Intent(WelcomeScreenActivity5.this, HomeActivity.class);
                 startActivity(i);
             }
         });
