aecd7bb54fac8f9ad1666ffd4722de3a5776dd3a
Dankyel Cordero <dankielcordero@gmail.com>
2017-05-18 19:26:04 +0200

    commit3

--- app/src/main/java/cz/inovett/bmicalendar/HomeActivity.java
+++ app/src/main/java/cz/inovett/bmicalendar/HomeActivity.java
@@ -1,19 +1,54 @@
 package cz.inovett.bmicalendar;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class HomeActivity extends AppCompatActivity {
+    private GraphView graph;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        graph = (GraphView) findViewById(R.id.graph);
+        setSeries1();
+        setSeries2();
+
 
     }
+
+    private void setSeries1() {
+        LineGraphSeries<DataPoint> series1 = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series1);
+    }
+
+    private void setSeries2() {
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, 2),
+                new DataPoint(1, 6),
+                new DataPoint(2, 4),
+                new DataPoint(3, 3),
+                new DataPoint(4, 7)
+        });
+        series2.setColor(Color.rgb(231, 76, 60));
+        graph.addSeries(series2);
+    }
+
 
     public void openCalendar(View view) {
         Intent i = new Intent(HomeActivity.this, CalendarActivity.class);
